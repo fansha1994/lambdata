2458a7be0fe9154d9596c2d4e06213dc2d80af6b
Mahmoud Fansha <fanasheh@hotmail.com>
2020-09-03 19:52:00 -0700

    Adding initial palceholder for package

---
+++ setup.py
@@ -0,0 +1,30 @@
+""" lambdata -  collection of data science helper function
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="lambdata-moe",
+    version="0.0.3",
+    author="fansha1994",
+    description="A collection of Data Science helper function",
+    long_description=LONG_DESCRIPTION,
+    long_description_content="text/markdown",
+    url="https://github.com/fansha1994/lambdata",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+) 
